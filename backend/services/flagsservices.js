405d6c40e390fd7dfdb610287d61017707ae6a3d
Manal Batta <manal.batta.1234@gmail.com>
2025-07-22 08:48:11 +0300

    Flagged question Notification send

--- backend/services/flagsservices.js
+++ backend/services/flagsservices.js
@@ -2,6 +2,7 @@ const FlagModel = require("../models/Flags");
 const QuestionModel = require("../models/Questions");
 const AnswerModel = require("../models/Answers");
 const { v4: uuidv4 } = require("uuid");
+const notificationService = require("./notificationService");
 class FlagServices {
   static async SubmitFlag(data) {
     try {
@@ -24,6 +25,25 @@ class FlagServices {
         { answerId: data.itemId },
         { isFlagged: true }
       );
+
+      // Send notification to question owner if a question is flagged
+      if (data.itemType === "question") {
+        const question = await QuestionModel.findOne({
+          questionId: data.itemId,
+        });
+        if (question && question.askedBy) {
+          await notificationService.sendNotification({
+            userId: question.askedBy,
+            type: "question_flagged",
+            title: "Your question was reported",
+            message:
+              "Your question has been reported and is under review by our team.",
+            data: { questionId: data.itemId },
+            saveToDatabase: true,
+          });
+        }
+      }
+
       return { newFlag };
     } catch (err) {
       throw err;
