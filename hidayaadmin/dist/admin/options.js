c6fb76631573723e8756ff12f6fbd50b19dcf94f
Manal Batta <manal.batta.1234@gmail.com>
2025-07-24 17:16:19 +0300

    admin page using adminjs

---
+++ hidayaadmin/dist/admin/options.js
@@ -0,0 +1,9 @@
+import UserModel from '../../../backend/models/User.js';
+import componentLoader from './component-loader.js';
+const options = {
+    componentLoader,
+    rootPath: '/admin',
+    resources: [UserModel],
+    databases: [],
+};
+export default options;
