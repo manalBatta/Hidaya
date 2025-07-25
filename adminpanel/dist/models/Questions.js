5d300c3abf79799836f9ce1651605cd23e3bd5ef
Manal Batta <manal.batta.1234@gmail.com>
2025-07-25 13:47:53 +0300

    Structure of Admin Panel

---
+++ adminpanel/dist/models/Questions.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+const questionSchema = new Schema({
+    questionId: { type: String, required: true, unique: true },
+    text: { type: String, required: true },
+    isPublic: { type: Boolean, default: true },
+    askedBy: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    aiAnswer: { type: String },
+    topAnswerId: { type: String },
+    tags: [{ type: String }],
+    category: { type: String },
+    isFlagged: { type: Boolean, default: false },
+});
+export default mongoose.model("Question", questionSchema, "Questions");
